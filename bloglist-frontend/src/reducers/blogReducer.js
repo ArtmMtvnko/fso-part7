5ac2ea63061fa2d90edf9cfb7113f6be126c0e5a
Artem <artemm3note@gmail.com>
2024-07-31 18:03:44 +0300

    Added redux toolkit for app. Created base reducer, still didn't test it.

---
+++ bloglist-frontend/src/reducers/blogReducer.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+import blogService from '../services/blogs'
+
+const blogSlice = createSlice({
+    name: 'blogs',
+    initialState: [],
+    reducers: {
+        setBlogs(state, action) {
+            return action.payload
+        },
+        appendBlog(state, action) {
+            return [...state, action.payload]
+        },
+    },
+})
+
+const { setBlogs, appendBlog } = blogSlice.actions
+
+export const initializeBlogs = () => {
+    return async (dispatch) => {
+        const blogs = await blogService.getAll()
+        dispatch(setBlogs(blogs))
+    }
+}
+
+export default blogSlice.reducer
